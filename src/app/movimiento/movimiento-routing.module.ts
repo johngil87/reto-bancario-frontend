e748d5783ce14b9ee245678ff6aa0c42d8e10947
John Gil Nuñez <john.gil@sofka.com.co>
2023-06-14 15:31:24 -0500

    se configura la carga perezosa de paginas

--- src/app/movimiento/movimiento-routing.module.ts
+++ src/app/movimiento/movimiento-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovimientoListComponent } from './movimiento-list/movimiento-list.component';
+import { MovimientoComponent } from './movimiento/movimiento.component';
+import { MovimientoNewComponent } from './movimiento-new/movimiento-new.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component: MovimientoListComponent
+},{
+  path:'move/:id',
+  component: MovimientoComponent
+},{
+  path:'newmove',
+  component: MovimientoNewComponent
+},];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
